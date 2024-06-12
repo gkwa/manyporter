902385d7adb611dc1ebe964cae459c84c0afe5d1
Taylor Monacelli <taylormonacelli@gmail.com>
2024-06-12 14:54:21 -0700

    Rename param to run from hello

--- cmd/run.go
+++ cmd/run.go
@@ -5,9 +5,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// helloCmd represents the hello command
-var helloCmd = &cobra.Command{
-	Use:   "hello",
+// runCmd represents the run command
+var runCmd = &cobra.Command{
+	Use:   "run",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -21,15 +21,15 @@ to quickly create a Cobra application.`,
 }
 
 func init() {
-	rootCmd.AddCommand(helloCmd)
+	rootCmd.AddCommand(runCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// helloCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// runCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// helloCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// runCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
