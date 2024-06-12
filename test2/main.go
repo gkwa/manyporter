cd6a177de93fabab4d441afede69d74e46d7e930
Taylor Monacelli <taylormonacelli@gmail.com>
2024-06-12 15:12:49 -0700

    Add test2

---
+++ test2/main.go
@@ -0,0 +1,30 @@
+package test2
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func Run() {
+	// map data
+	mapData := map[string]interface{}{
+		"Name":    "noknow",
+		"Age":     2,
+		"Admin":   true,
+		"Hobbies": []string{"IT", "Travel"},
+		"Address": map[string]interface{}{
+			"PostalCode": 1111,
+			"Country":    "Japan",
+		},
+		"Null": nil,
+	}
+
+	// Convert map to json string
+	jsonStr, err := json.MarshalIndent(mapData, "", "  ")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Output
+	fmt.Println(string(jsonStr))
+}
