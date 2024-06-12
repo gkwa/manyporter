ce28513eb804d6a3b4778e5700225b802457817c
Taylor Monacelli <taylormonacelli@gmail.com>
2024-06-12 15:41:18 -0700

    readme

--- README.md
+++ README.md
@@ -6,6 +6,55 @@ Use goldmark to fetch markdown frontmatter as json.
 Motivation:
 Feed my markdown notes into [quickwit](https://quickwit.io/)
 
+## 
+
+
+```bash
+
+./manyporter test3
+
+```
+
+this gives error:
+
+```
+MarshalIndent failed json: unsupported type: map[interface {}]interface {}
+```
+
+It is for that reason why we need more. test1 works:
+
+
+```
+
+./manyporter test1
+
+```
+
+works:
+
+
+```log
+
+[mtm@ttorres-t14s:manyporter(master)]$ ./manyporter test1
+{
+  "filetype": "product",
+  "x": {
+    "z": {
+      "n": {
+        "p": 10,
+        "z": [
+          "test",
+          2
+        ]
+      }
+    }
+  }
+}
+
+```
+
+
+
 ## example usage
 
 ```bash
--- test3/main.go
+++ test3/main.go
@@ -3,26 +3,35 @@ package test3
 import (
 	"encoding/json"
 	"fmt"
+	"os"
+
+	"github.com/yuin/goldmark"
+	meta "github.com/yuin/goldmark-meta"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/text"
 )
 
 func Run() {
-	// map data
-	mapData := map[string]interface{}{
-		"Name":    "noknow",
-		"Age":     2,
-		"Admin":   true,
-		"Hobbies": []string{"IT", "Travel"},
-		"Address": map[string]interface{}{
-			"PostalCode": 1111,
-			"Country":    "Japan",
-		},
-		"Null": nil,
+	var mapData map[string]interface{}
+
+	filename := "test1/example.md"
+	markdownBytes, err := os.ReadFile(filename)
+	if err != nil {
+		panic(err)
 	}
+
+	md := goldmark.New(goldmark.WithExtensions(meta.Meta))
+	context := parser.NewContext()
+
+	md.Parser().Parse(text.NewReader(markdownBytes), parser.WithContext(context))
+	mapData = meta.Get(context)
 
 	// Convert map to json string
 	jsonStr, err := json.MarshalIndent(mapData, "", "  ")
 	if err != nil {
-		fmt.Println(err)
+		err1 := fmt.Errorf("MarshalIndent failed %w", err)
+		fmt.Println(err1)
+		return
 	}
 
 	// Output
