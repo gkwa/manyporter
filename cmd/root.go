db79f087f3930ac8c20bbd099d2e91d9f2f1f55b
Taylor Monacelli <taylormonacelli@gmail.com>
2024-06-12 14:53:06 -0700

    reduce to no need for cli param

--- cmd/root.go
+++ cmd/root.go
@@ -5,6 +5,7 @@ import (
 	"log/slog"
 	"os"
 
+	"github.com/gkwa/manyporter/core"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"github.com/taylormonacelli/goldbug"
@@ -28,9 +29,9 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) {
-	// 	fmt.Println("Hello from manyporter!")
-	// },
+	Run: func(cmd *cobra.Command, args []string) {
+		core.Run()
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
